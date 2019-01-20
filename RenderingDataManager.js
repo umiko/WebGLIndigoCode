d0445f39928528648ff797236cacb50a8d356c10
Umiko <robertscholl.private@gmail.com>
2019-01-20 13:20:07 +0100

    Rendering Manager now has shader compilation capabilities and can validate shaders, still unsure how i want to load the files though.

--- RenderingDataManager.js
+++ RenderingDataManager.js
@@ -7,13 +7,14 @@ class RenderingDataManager{
     }
 
     loadShaderCodeFromFiles(ShaderFilePathArray){
-        this.shaderCodeArray = Array(ShaderFilePathArray.length);
+        let shaderCodeArray = Array(ShaderFilePathArray.length);
         ShaderFilePathArray.map(filePath => loadTextResourceFromFile(filePath,function(err, result){
             if(err)
                 console.error(err);
             else
-                this.shaderCodeArray.push(result);
+                shaderCodeArray.push(result);
         }));
+        return shaderCodeArray;
     }
 
     getCurrentShader(){
@@ -30,4 +31,51 @@ class RenderingDataManager{
         filePair['fragmentShaderFilePath'] = fragmentShaderFilePath;
         return filePair;
     }
+
+    #createShaderProgram(vertexCode, fragmentCode){
+        let vertexShader = this.#compileShader(vertexCode, this.context.VERTEX_SHADER);
+        let fragmentShader = this.#compileShader(fragmentCode, this.context.FRAGMENT_SHADER);
+        let shaderProgram = this.#linkShaderProgram(vertexShader, fragmentShader);
+        this.#validateShaderProgram(shaderProgram);
+        return shaderProgram;
+    }
+
+    #compileShader(shaderCode, shaderType){
+        let compiledShader = this.context.createShader(shaderType);
+        this.context.shaderSource(compiledShader, shaderCode);
+        this.context.compileShader(compiledShader);
+        this.#checkShaderCompilationStatus(compiledShader);
+        return compiledShader;
+    }
+
+    #linkShaderProgram(vertexShader, fragmentShader){
+        let shaderProgram = this.context.createProgram();
+        this.context.attachShader(shaderProgram, vertexShader);
+        this.context.attachShader(shaderProgram, fragmentShader);
+        this.context.linkProgram(shaderProgram);
+        this.#checkProgramLinkStatus(shaderProgram);
+        return shaderProgram;
+    }
+
+    //<editor-fold desc="Shader Validation">
+
+    #checkShaderCompilationStatus(compiledShader){
+        if(!this.context.getShaderParameter(compiledShader, this.context.COMPILE_STATUS)){
+            throw new Error("Error in Shader Compilation!\n"+ this.context.getShaderInfoLog(compiledShader));
+        }
+    }
+
+    #checkProgramLinkStatus(shaderProgram) {
+        if(!this.context.getProgramParameter(shaderProgram, this.context.LINK_STATUS)){
+            throw new Error("Error linking program!\n" + this.context.getProgramInfoLog(shaderProgram));
+        }
+    }
+
+    #validateShaderProgram(shaderProgram){
+        if(!this.context.getProgramParameter(shaderProgram, this.context.VALIDATE_STATUS)){
+           throw new Error("Error validating program!\n" + this.context.getProgramInfoLog(shaderProgram));
+        }
+    }
+    //</editor-fold>
+
 }
